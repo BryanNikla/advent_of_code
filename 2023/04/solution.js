9ec54ccee11df5b6b98e9f745de5968736e08e93
Bryan Nikla <bryannikla@gmail.com>
2023-12-04 02:30:03 -0500

    2023/04 - Some more changes

--- 2023/04/solution.js
+++ 2023/04/solution.js
@@ -1,7 +1,7 @@
 // 2023/04
 module.exports = {one, two, solutions: [13, 30]};
 
-const {arraySum, arrOfLength} = require(global.UTILITIES_PATH);
+const {arraySum, forX} = require(global.UTILITIES_PATH);
 
 function parseInputIntoCardValues(inputLines) {
     const _getNumbers = (str) => {
@@ -12,20 +12,19 @@ function parseInputIntoCardValues(inputLines) {
             .map((n) => Number(n));
     };
     return inputLines.map((line) => {
-        const [_, val] = line.split(":");
-        const [winners, numbers] = val.split("|");
+        const [winners, numbers] = line.split(":")[1].split("|");
         return [_getNumbers(winners), _getNumbers(numbers)];
     });
 }
 
-function numberOfWinningNumbers(winners, numbers) {
+function wins(winners, numbers) {
     return winners.filter((value) => numbers.includes(value)).length;
 }
 
 function one(input) {
     return arraySum(
         parseInputIntoCardValues(input.split("\n")).map(([winners, numbers]) => {
-            const count = numberOfWinningNumbers(winners, numbers);
+            const count = wins(winners, numbers);
             return count <= 0 ? 0 : 1 * Math.pow(2, count - 1);
         })
     );
@@ -33,15 +32,9 @@ function one(input) {
 
 function two(input) {
     const cards = parseInputIntoCardValues(input.split("\n"));
-    const pileCount = cards.map(() => 1);
-    cards.forEach(([winners, numbers], i) => {
-        arrOfLength(numberOfWinningNumbers(winners, numbers)).forEach((x) => {
-            if (pileCount[i + x + 1]) {
-                pileCount[i + x + 1] += pileCount[i];
-            }
-        });
-    });
-    return arraySum(pileCount);
+    const count = cards.map(() => 1);
+    cards.forEach(([w, n], i) => forX(wins(w, n), (x) => (count[i + x] += count[i])));
+    return arraySum(count);
 
     // OLD SOLVE (slow, needed to be refactored)
     // Keeping it just for reference
@@ -52,7 +45,7 @@ function two(input) {
     //         const cardsInPile = pile.length;
     //         while (pile.length) {
     //             const [winners, numbers] = pile.pop();
-    //             for (let w = numberOfWinningNumbers(winners, numbers); w > 0; w--) {
+    //             for (let w = wins(winners, numbers); w > 0; w--) {
     //                 if (pilesByIndex[i + w]) {
     //                     pilesByIndex[i + w].push(structuredClone(pilesByIndex[i + w][0]));
     //                 }
