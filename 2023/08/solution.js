5686870f32e2e3e1ab67ef958c57ad308f612743
Bryan Nikla <bryannikla@gmail.com>
2023-12-08 14:05:33 -0500

    2023/08 - Refactored part of my solution to use built-in Generator object

--- 2023/08/solution.js
+++ 2023/08/solution.js
@@ -3,37 +3,44 @@ module.exports = {one, two, solutions: [6, 6]};
 function parseInput(inputString) {
     const [steps, _, ...mapLines] = inputString.split("\n");
     return {
-        steps: steps.split(""),
+        stepsArray: steps.split(""),
         map: new Map(mapLines.map((s) => s.split(" = ")).map(([k, str]) => [k, str.replaceAll("(", "").replaceAll(")", "").split(", ")])),
     };
 }
 
-function useSteps(steps = []) {
+/**
+ * @description Generator function that yields the next step indefinately
+ * @param {Array} stepArray
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Generator
+ */
+function* steps(stepArray = []) {
     let current = -1;
-    return {next: () => (steps[++current % steps.length] === "L" ? 0 : 1)};
+    while (true) {
+        yield stepArray[++current % stepArray.length] === "L" ? 0 : 1;
+    }
 }
 
 function one(input) {
-    const {steps, map} = parseInput(input);
-    const {next} = useSteps(steps);
+    const {stepsArray, map} = parseInput(input);
+    const stepGenerator = steps(stepsArray);
     let [i, pos] = [-1, "AAA"];
     while (-1 < ++i && pos !== "ZZZ") {
-        pos = map.get(pos)[next()];
+        pos = map.get(pos)[stepGenerator.next().value];
     }
     return i;
 }
 
 function two(input) {
     const {leastCommonMultiple} = require(global.UTILITIES_PATH);
-    const {steps, map} = parseInput(input);
+    const {stepsArray, map} = parseInput(input);
     return leastCommonMultiple(
         Array.from(map.keys())
             .filter((str) => str.endsWith("A"))
             .map((key) => {
-                const {next} = useSteps(steps);
+                const stepGenerator = steps(stepsArray);
                 let [i, pos] = [-1, key];
                 while (-1 < ++i && !pos.endsWith("Z")) {
-                    pos = map.get(pos)[next()];
+                    pos = map.get(pos)[stepGenerator.next().value];
                 }
                 return i;
             })
